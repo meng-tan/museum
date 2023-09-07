4cc6d62ef9dccf64548d8b0a991ff92edc401a62
TM <tanmeng_job@qq.com>
2023-09-07 09:01:40 -0400

    cache results of preflight requests for 24 hours

--- museum-api/server.js
+++ museum-api/server.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Expose-Headers", "Token");
-  res.header("Access-Control-Max-Age", 600);
+  res.header("Access-Control-Max-Age", 86400);
   next();
 });
 
